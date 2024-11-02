a30208961d773a81db2d7d0b552e2d8fdd7409df
G0C001 <gokulvbe1303@gmail.com>
2024-11-03 00:28:07 +0530

    first

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,13 @@
+# Create and activate a new virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
+# Upgrade pip to the latest version to avoid issues with dependencies
+pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
 
-python -m pip install playwright
+# Install Playwright and its browsers
+pip install playwright
 python -m playwright install
