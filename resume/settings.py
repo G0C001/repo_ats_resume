0d57b649339ca752b8db38d602895bb332a37e4a
G0C001 <gokulvbe1303@gmail.com>
2024-11-02 19:28:48 +0530

    first commit

--- resume/settings.py
+++ resume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ac#ji)m6e5185#oy@+4&yz(+x$rx(f2@0=5dufjlj6ano$!6x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1','localhost']
 
 
 # Application definition
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
