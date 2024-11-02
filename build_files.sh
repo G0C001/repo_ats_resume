804318be504b0c0da059dd75c69fa44da2a80e36
G0C001 <gokulvbe1303@gmail.com>
2024-11-03 00:41:29 +0530

    first

--- build_files.sh
+++ build_files.sh
@@ -1,26 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-# Create and activate a new virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-# Upgrade pip to the latest version to avoid issues with dependencies
-pip install --upgrade pip
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Update package lists and install necessary system dependencies
-apt-get update
-apt-get install -y libnss3 libnspr4 libgbm1
-
-# Install Playwright and its browsers
-pip install playwright
-playwright install-deps
-
-# Optionally, you can deactivate the virtual environment after installation
-# deactivate
-
-
-# Install necessary dependencies
-
+# Check if apt-get is available
+if command -v apt-get > /dev/null; then
+    apt-get update && apt-get install -y libnss3 libnspr4 libgbm1
+elif command -v apk > /dev/null; then
+    apk add --no-cache libnss3 libnspr4 libgbm1
+elif command -v yum > /dev/null; then
+    yum install -y nss nspr libgbm
+else
+    echo "No compatible package manager found. Please install the dependencies manually."
+    exit 1
+fi
