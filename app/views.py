6f826d3b945d27b728e7d7a97cedc23aa954875d
G0C001 <gokulvbe1303@gmail.com>
2024-11-03 00:06:57 +0530

    first

--- app/views.py
+++ app/views.py
@@ -1,51 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from app.body import styleresume, personal, summary, education, experience_certfy, skills, project_lang
-import asyncio
-from playwright.async_api import async_playwright
+# import asyncio
+# from playwright.async_api import async_playwright
 
-async def generate_pdf(html_content):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
+# async def generate_pdf(html_content):
+#     async with async_playwright() as p:
+#         browser = await p.chromium.launch()
+#         page = await browser.new_page()
 
-        # Set the HTML content
-        await page.set_content(html_content)
+#         # Set the HTML content
+#         await page.set_content(html_content)
 
-        # Generate PDF
-        pdf_file = await page.pdf(format='A4')
+#         # Generate PDF
+#         pdf_file = await page.pdf(format='A4')
 
-        await browser.close()
+#         await browser.close()
 
-        return pdf_file
+#         return pdf_file
     
 def resume(request):
     if request.method == 'POST':
-        # Example data retrieval from POST request
-        role = request.POST.get('Role')
-        skill = request.POST.get('skill')
+        pass
+        # role = request.POST.get('Role')
+        # skill = request.POST.get('skill')
 
-        # Generate HTML content for the PDF
-        html_template = f"""
-                        {styleresume.style()}
-                        <body>
-                            <div class="container">
-                                {personal.personal(role)}
-                                {summary.summary(role)}
-                                {education.education()}
-                                {experience_certfy.experience_certfy()}
-                                {skills.skill(skill)}
-                                {project_lang.project_lan()}
-                            </div>
-                        </body>
-                    """
+        # # Generate HTML content for the PDF
+        # html_template = f"""
+        #                 {styleresume.style()}
+        #                 <body>
+        #                     <div class="container">
+        #                         {personal.personal(role)}
+        #                         {summary.summary(role)}
+        #                         {education.education()}
+        #                         {experience_certfy.experience_certfy()}
+        #                         {skills.skill(skill)}
+        #                         {project_lang.project_lan()}
+        #                     </div>
+        #                 </body>
+        #             """
 
 
-        pdf_file = asyncio.run(generate_pdf(html_template))
+        # pdf_file = asyncio.run(generate_pdf(html_template))
 
-        response = HttpResponse(pdf_file, content_type='application/pdf')
-        response['Content-Disposition'] = f'inline; filename="GokulVasanth_{role}_Resume.pdf"'
+        # response = HttpResponse(pdf_file, content_type='application/pdf')
+        # response['Content-Disposition'] = f'inline; filename="GokulVasanth_{role}_Resume.pdf"'
 
-        return response
+        # return response
 
     return render(request, 'index.html')
