5b29f47ea3c2686290d1039b91eb86b5559f0cc2
G0C001 <gokulvbe1303@gmail.com>
2024-11-03 15:25:23 +0530

    main

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from app.body import styleresume, personal, summary, education, experience_certfy, skills, project_lang
 
 def resume(request):
