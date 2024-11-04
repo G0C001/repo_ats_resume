3aaec60324a79d216d4b5333338733397e39ebf2
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 04:45:53 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,16 @@
-pip3 install -r requirements.txt
-apt install libgobject-2.0-0 libpango-1.0-0 libfontconfig1 libpangoft2-1.0-0
+python3 -m venv myenv
+source myenv/bin/activate
+pip install -r requirements.txt
+apt update
+apt install -y libgobject-2.0-0 libpango-1.0-0 libfontconfig1 libpangoft2-1.0-0
 
+# Install pip if not installed
+if ! command -v pip3 &> /dev/null
+then
+    apt install -y python3-pip
+fi
+
+# Install Python packages from requirements.txt
+pip3 install -r requirements.txt
 
 echo "Setup completed successfully!"
