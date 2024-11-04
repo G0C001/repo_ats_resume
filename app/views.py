eece0f2abad963651f2b7062e09d61057307f0ac
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 04:40:12 +0530

    commit

--- app/views.py
+++ app/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, redirect
 from app.body import styleresume, personal, summary, education, experience_certfy, skills, project_lang
 from django.http import HttpResponse
 import json
-import ctypes
-import os
-dll_directory = r"./GTK3"
-for file_name in os.listdir(dll_directory):
-    if file_name.endswith(".dll"):
-        dll_path = os.path.join(dll_directory, file_name)
-        try:
-            my_dll = ctypes.CDLL(dll_path)
-        except Exception as e:
-            print(f"Failed to load {dll_path}: {e}")
-from weasyprint import HTML
+# import ctypes
+# import os
+# dll_directory = r"./GTK3"
+# for file_name in os.listdir(dll_directory):
+#     if file_name.endswith(".dll"):
+#         dll_path = os.path.join(dll_directory, file_name)
+#         try:
+#             my_dll = ctypes.CDLL(dll_path)
+#         except Exception as e:
+#             print(f"Failed to load {dll_path}: {e}")
+# from weasyprint import HTML
 
 def resume(request):
     if request.method == 'POST':
@@ -42,13 +42,13 @@ def resume(request):
                         </html>
                     """
          # Create a PDF response
-        pdf = HTML(string=html_content).write_pdf()
+        # pdf = HTML(string=html_content).write_pdf()
         
         # Set up the response
-        response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename="Gokul_Vasanth_{role}_Resume.pdf"'
+        # response = HttpResponse(pdf, content_type='application/pdf')
+        # response['Content-Disposition'] = f'attachment; filename="Gokul_Vasanth_{role}_Resume.pdf"'
         
-        return response
+        return 'response'
 
     return render(request, 'index.html')
 
--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 pip3 install -r requirements.txt
+sudo apt install libgobject-2.0-0 libpango-1.0-0 libfontconfig1 libpangoft2-1.0-0
 
 echo "Setup completed successfully!"
