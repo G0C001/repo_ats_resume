28e61a720d593ebeffa4759250324ee1ba90a480
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 10:53:57 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
-# python3 -m venv myenv
-# source myenv/bin/activate
-# pip install -r requirements.txt
+python3 -m venv myenv
+source myenv/bin/activate
+pip install -r requirements.txt
 
-dnf install -y libgobject-2.0.so libpango-1.0.so libfontconfig.so libpangoft2-1.0.so
-echo "dll installed"
+# Update the system and install necessary development libraries using dnf
+sudo dnf update -y
+sudo dnf install -y glib2 pango fontconfig
+
 pip3 install django Weasyprint
 
 echo "Setup completed successfully!"
