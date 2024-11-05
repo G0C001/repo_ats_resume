eceb326f319540849770acc771437a3467d41b9d
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 10:46:43 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,7 @@ python3 -m venv myenv
 source myenv/bin/activate
 pip install -r requirements.txt
 
-sudo dnf update -y
-sudo dnf install -y libgobject-2.0.so libpango-1.0.so libfontconfig.so libpangoft2-1.0.so
+dnf install -y libgobject-2.0.so libpango-1.0.so libfontconfig.so libpangoft2-1.0.so
 
 pip3 install django Weasyprint
 
