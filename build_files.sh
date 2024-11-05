236262846326d33ba7ccc9ef4c5fd30a13eae050
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 11:42:31 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-# python3 -m venv myenv
-# source myenv/bin/activate
+python3 -m venv myenv
+source myenv/bin/activate
 # pip install -r requirements.txt
 
 # dnf install -y libgobject-2.0.so libpango-1.0.so libfontconfig.so libpangoft2-1.0.so
